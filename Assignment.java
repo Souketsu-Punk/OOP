5fc4775eaad8058f1f81b10ab3a61ebe373264db
Souketsu-Punk <137512954+Souketsu-Punk@users.noreply.github.com>
2025-11-26 14:15:41 +0300

    Delete Assignment.java

--- Assignment.java
+++
@@ -1,7 +0,0 @@
-public class Assignment {
-    public static void main(String[] args) {
-        int count = 0;
-        count = count +1;
-        System.out.println("count is: " + count);
-    }
-}
